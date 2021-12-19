9abe670694112858ab3d32b7f59c10b2a9f1c37f
Yannso <48429847+Yannso@users.noreply.github.com>
2021-12-19 14:50:31 +0800

    Add files via upload

---
+++ Graph/Lc997.java
@@ -0,0 +1,17 @@
+public class Lc997 {
+    public int findJudge(int n, int[][] trust) {
+        int[] inbound = new int[n+1];
+        int[] outbound = new int[n+1];
+        for (int[] t : trust) {
+            int x = t[0], y = t[1];
+            outbound[x]++;
+            inbound[y]++;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            if (outbound[i] == 0 && inbound[i] == n - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
